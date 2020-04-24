2548a895033cd89197097f33ca95bfd525f3838a
zurqonisyihab <zurqonimuhammad@gmail.com>
2020-04-24 02:58:14 -0700

    Project IDEA

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,14 @@
 public class Main {
-}
+        public static void main(String[] args) {
+            String namaDepan = "Muhammad";
+            String namaTengah = "Zurqoni";
+            String namaBelakang = "Sihab";
+            int usia = 18;
+            int targetTahunKuliah = 4;
+            double ipk = 3.8976512;
+            char nilaiAbjad = 'A';
+            boolean tampan = true;
+
+            System.out.println(namaDepan + namaTengah + namaBelakang);
+        }
+    }
